92b965ca3a24e6a96cac389fa025c9ef0ceb45a0
yut23 <yut23@gvljohnsons.com>
2021-12-04 23:08:37 -0500

    Restructure day 4

--- src/day4.py
+++ src/day4.py
@@ -1,20 +1,22 @@
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
 import numpy.typing as npt
 
 
 class Bingo:
-    def __init__(self, boards: List[List[str]]):
-        self.numbers: npt.NDArray[int] = np.array(
-            [[l.split() for l in lines] for lines in boards], dtype=int
-        )
-        self.marked: npt.NDArray[bool] = np.zeros_like(self.numbers, dtype=bool)
-        self.won: npt.NDArray[bool] = np.zeros(self.numbers.shape[0], dtype=bool)
+    def __init__(self, lines: List[str]):
+        self.order = [int(n) for n in lines[0].split(",")]
+        boards = []
+        for i in range(2, len(lines) - 1, 6):
+            boards.append([l.split() for l in lines[i : i + 5]])
+        self.boards: npt.NDArray[int] = np.array(boards, dtype=int)
+        self.marked: npt.NDArray[bool] = np.zeros_like(self.boards, dtype=bool)
+        self.won: npt.NDArray[bool] = np.zeros(self.boards.shape[0], dtype=bool)
 
     def mark(self, number: int) -> List[int]:
         """Returns the scores of all winning boards."""
-        self.marked[~self.won] |= self.numbers[~self.won] == number
+        self.marked[~self.won] |= self.boards[~self.won] == number
         new_wins: npt.NDArray[bool] = ~self.won & (
             self.marked.prod(axis=1).any(axis=-1)
             | self.marked.prod(axis=2).any(axis=-1)
@@ -22,22 +24,14 @@ class Bingo:
         if new_wins.any():
             self.won[new_wins] = True
             mask = ~self.marked[new_wins]
-            scores = self.numbers[new_wins].sum(axis=(1, 2), where=mask) * number
+            scores = self.boards[new_wins].sum(axis=(1, 2), where=mask) * number
             return list(scores)
         return []
 
 
-def read_boards(lines: List[str]) -> Tuple[List[int], Bingo]:
-    order = [int(n) for n in lines[0].split(",")]
-    boards = []
-    for i in range(2, len(lines) - 1, 6):
-        boards.append(lines[i : i + 5])
-    return order, Bingo(boards)
-
-
 def part_1(lines: List[str]) -> int:
-    order, bingo = read_boards(lines)
-    for num in order:
+    bingo = Bingo(lines)
+    for num in bingo.order:
         new_wins = bingo.mark(num)
         if new_wins:
             return new_wins[0]
@@ -45,8 +39,8 @@ def part_1(lines: List[str]) -> int:
 
 
 def part_2(lines: List[str]) -> int:
-    order, bingo = read_boards(lines)
-    for num in order:
+    bingo = Bingo(lines)
+    for num in bingo.order:
         new_wins = bingo.mark(num)
         if bingo.won.all():
             return new_wins[0]
