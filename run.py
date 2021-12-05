98bb4b521a87570be27da056f4ee088950d3074a
yut23 <yut23@gvljohnsons.com>
2021-12-04 21:57:13 -0500

    Future-proof the current day selection

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import sys
 from pathlib import Path
 
 ROOT = Path(__file__).parent.resolve()
-TODAY = datetime.datetime.now().day
+TODAY = min(datetime.datetime.now(), datetime.datetime(2021, 12, 25)).day
 
 # add the source directory to the beginning of Python's search path
 sys.path.insert(0, str(ROOT / "src"))
