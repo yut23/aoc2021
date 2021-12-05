ab9ed9495f6f0fb5e57023b557b74a04e0e798ed
yut23 <yut23@gvljohnsons.com>
2021-12-04 21:56:30 -0500

    Run on example inputs with -e

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import datetime
 import importlib
 import sys
 from pathlib import Path
-from typing import Optional, TextIO
 
 ROOT = Path(__file__).parent.resolve()
 TODAY = datetime.datetime.now().day
@@ -14,32 +13,30 @@ TODAY = datetime.datetime.now().day
 sys.path.insert(0, str(ROOT / "src"))
 
 
-def run_day(n: int, file: Optional[TextIO] = None) -> None:
-    if file is None:
-        file = open(ROOT / f"input/day{n}.txt")  # pylint: disable=consider-using-with
-    try:
-        lines = file.read().splitlines(keepends=False)
-    finally:
-        file.close()
-
-    day = importlib.import_module(f"day{n}")
-
-    print(f"Day {n}")
-    if hasattr(day, "part_1"):
-        answer = day.part_1(lines)  # type: ignore
-        print(f"Part 1: {answer}")
-
-    if hasattr(day, "part_2"):
-        answer = day.part_2(lines)  # type: ignore
-        print(f"Part 2: {answer}")
-
-
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input", type=argparse.FileType("r"), help="Override the input file"
+    input_group = parser.add_mutually_exclusive_group()
+    input_group.add_argument(
+        "-e",
+        "--example",
+        action="store_const",
+        dest="input_dir",
+        const="example",
+        default="input",
+        help="use the example input from the instructions",
     )
-    parser.add_argument("day", nargs=argparse.ZERO_OR_MORE, help="What days to run")
+    input_group.add_argument(
+        "-i",
+        "--input",
+        metavar="file",
+        type=argparse.FileType("r"),
+        help="override the input file",
+    )
+    parser.add_argument(
+        "day",
+        nargs=argparse.ZERO_OR_MORE,
+        help="a day number to run (defaults to today)",
+    )
     args = parser.parse_args()
 
     if args.day:
@@ -54,7 +51,25 @@ def main() -> None:
         parser.error("-i/--input cannot be used if multiple days are specified")
 
     for i, n in enumerate(days):
-        run_day(n, args.input)
+        file = args.input
+        if file is None:
+            file = open(ROOT / args.input_dir / f"day{n}.txt")
+        try:
+            lines = file.read().splitlines(keepends=False)
+        finally:
+            file.close()
+
+        day = importlib.import_module(f"day{n}")
+
+        print(f"Day {n}")
+        if hasattr(day, "part_1"):
+            answer = day.part_1(lines)  # type: ignore
+            print(f"Part 1: {answer}")
+
+        if hasattr(day, "part_2"):
+            answer = day.part_2(lines)  # type: ignore
+            print(f"Part 2: {answer}")
+
         if i != len(days) - 1:
             print()
 
