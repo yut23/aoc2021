4baecc959c36c31b4d55c37e9d2b53b4eae0f935
yut23 <yut23@gvljohnsons.com>
2021-12-04 22:55:22 -0500

    Add -t to run through timeit

--- run.py
+++ run.py
@@ -3,8 +3,13 @@
 import argparse
 import datetime
 import importlib
+import math
 import sys
+import timeit
 from pathlib import Path
+from typing import Any, Dict, Optional
+
+import numpy as np
 
 ROOT = Path(__file__).parent.resolve()
 TODAY = min(datetime.datetime.now(), datetime.datetime(2021, 12, 25)).day
@@ -38,6 +43,7 @@ def main() -> None:
         type=argparse.FileType("r"),
         help="override the input file",
     )
+    parser.add_argument("-t", "--timeit", action="store_true", help="time each part")
     parser.add_argument(
         "day",
         nargs=argparse.ZERO_OR_MORE,
@@ -58,7 +64,9 @@ def main() -> None:
     for i, n in enumerate(days):
         file = args.input
         if file is None:
-            file = open(ROOT / args.input_dir / f"day{n}.txt")
+            file = open(  # pylint: disable=consider-using-with
+                ROOT / args.input_dir / f"day{n}.txt", "r"
+            )
         try:
             lines = file.read().splitlines(keepends=False)
         finally:
@@ -70,13 +78,64 @@ def main() -> None:
         if hasattr(day, "part_1"):
             answer = day.part_1(lines)  # type: ignore
             print(f"Part 1: {answer}")
+            if args.timeit:
+                get_timing("day.part_1(lines)", globals={**locals(), **globals()})
 
         if hasattr(day, "part_2"):
             answer = day.part_2(lines)  # type: ignore
             print(f"Part 2: {answer}")
+            if args.timeit:
+                get_timing("day.part_2(lines)", globals={**locals(), **globals()})
 
         if i != len(days) - 1:
             print()
+
+
+# from IPython/core/magics/execution.py
+def _format_time(timespan: float, precision: int = 3) -> str:
+    """Formats the timespan in a human readable form"""
+
+    if timespan >= 60.0:
+        # we have more than a minute, format that in a human readable form
+        # Idea from http://snipplr.com/view/5713/
+        parts = [("d", 60 * 60 * 24), ("h", 60 * 60), ("min", 60), ("s", 1)]
+        time = []
+        leftover = timespan
+        for suffix, length in parts:
+            value = int(leftover / length)
+            if value > 0:
+                leftover = leftover % length
+                time.append(f"{value!s}{suffix}")
+            if leftover < 1:
+                break
+        return " ".join(time)
+
+    units = ["s", "ms", "μs", "ns"]
+    scaling = [1, 1e3, 1e6, 1e9]
+
+    if timespan > 0.0:
+        order = min(-int(math.floor(math.log10(timespan)) // 3), 3)
+    else:
+        order = 3
+    return f"{timespan * scaling[order]:.{precision}g} {units[order]}"
+
+
+def get_timing(
+    stmt: str = "pass",
+    setup: str = "pass",
+    repeat: int = 7,
+    globals: Optional[Dict[str, Any]] = None,
+) -> None:
+    timer = timeit.Timer(stmt, setup, globals=globals)
+    num_loops, total_time = timer.autorange()
+    times = (
+        np.array([total_time, *timer.repeat(repeat=repeat - 1, number=num_loops)])
+        / num_loops
+    )
+    print(
+        f"{_format_time(times.mean())} ± {_format_time(times.std())} per loop"
+        f" (mean ± std. dev. of {repeat} loops, {num_loops} loops each)\n"
+    )
 
 
 if __name__ == "__main__":
