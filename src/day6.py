f7c9cd1db9f9055abe38eed2c0b7766999b3b56c
yut23 <yut23@gvljohnsons.com>
2021-12-06 16:36:07 -0500

    Use a faster implementation for day 6

--- src/day6.py
+++ src/day6.py
@@ -21,11 +21,24 @@ def simulate(initial_timers: List[int], days: int) -> int:
     return sum(fish)
 
 
+def simulate_inplace(initial_timers: List[int], days: int) -> int:
+    # number of fish with each timer value (0 - 8, inclusive)
+    fish: List[int] = [0] * 9
+    for timer in initial_timers:
+        fish[timer] += 1
+
+    for _ in range(days):
+        fish[0:8], fish[8] = fish[1:9], fish[0]
+        fish[6] += fish[8]
+
+    return sum(fish)
+
+
 def part_1(lines: List[str]) -> int:
     initial_timers = [int(x) for x in lines[0].split(",")]
-    return simulate(initial_timers, 80)
+    return simulate_inplace(initial_timers, 80)
 
 
 def part_2(lines: List[str]) -> int:
     initial_timers = [int(x) for x in lines[0].split(",")]
-    return simulate(initial_timers, 256)
+    return simulate_inplace(initial_timers, 256)
