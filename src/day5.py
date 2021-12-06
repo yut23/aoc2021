ef5bfe06e928c16b74403a2ed2e508ea6feaab03
yut23 <yut23@gvljohnsons.com>
2021-12-05 23:59:27 -0500

    Day 5, part 2

--- src/day5.py
+++ src/day5.py
@@ -1,3 +1,4 @@
+import math
 from typing import Counter, Generator, List, Tuple
 
 
@@ -16,15 +17,22 @@ class Line:
                 (self.x1, y)
                 for y in range(min(self.y1, self.y2), max(self.y1, self.y2) + 1)
             )
-        elif self.is_horizontal():
+            return
+        if self.is_horizontal():
             yield from (
                 (x, self.y1)
                 for x in range(min(self.x1, self.x2), max(self.x1, self.x2) + 1)
             )
-        else:
-            raise NotImplementedError(
-                "Line.to_array() can only handle horizontal or vertical lines"
-            )
+            return
+
+        # diagonal line
+        dx = int(math.copysign(1, self.x2 - self.x1))
+        dy = int(math.copysign(1, self.y2 - self.y1))
+
+        yield from (
+            (self.x1 + i * dx, self.y1 + i * dy)
+            for i in range(abs(self.x2 - self.x1) + 1)
+        )
 
     def is_horizontal(self) -> bool:
         return self.y1 == self.y2
@@ -40,5 +48,15 @@ def part_1(input_lines: List[str]) -> int:
         if line.is_horizontal() or line.is_vertical():
             for point in line.get_points():
                 line_counts[point] += 1
+
+    return sum(int(c > 1) for c in line_counts.values())
+
+
+def part_2(input_lines: List[str]) -> int:
+    line_counts: Counter[Tuple[int, int]] = Counter()
+
+    for line in map(Line, input_lines):
+        for point in line.get_points():
+            line_counts[point] += 1
 
     return sum(int(c > 1) for c in line_counts.values())
