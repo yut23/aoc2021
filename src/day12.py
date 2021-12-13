5cfbc785ddf0e4aa1cd78512030252dc4b41e837
yut23 <yut23@gvljohnsons.com>
2021-12-12 21:46:04 -0500

    Day 12, part 2

--- src/day12.py
+++ src/day12.py
@@ -11,23 +11,38 @@ def parse(lines: List[str]) -> nx.Graph:
     return G
 
 
-def dfs_step(G: nx.Graph, path: List[str]) -> Generator[List[str], None, None]:
+def dfs_step(
+    G: nx.Graph, path: List[str], small_twice: str = ""
+) -> Generator[List[str], None, None]:
+    """Recursive DFS to find all paths through the graph."""
     node = path[-1]
     for u in G.neighbors(node):
         if u == "end":
+            # ensure small_twice appears in the path exactly twice, if it's not empty
+            if small_twice and path.count(small_twice) != 2:
+                continue
             yield [*path, u]
             continue
         if u.islower() and u in path:
-            continue
-        yield from dfs_step(G, [*path, u])
-
-
-def find_paths(G: nx.Graph) -> Generator[List[str], None, None]:
-    """Recursive DFS to find all paths through the graph."""
-    yield from dfs_step(G, ["start"])
+            if u == small_twice and path.count(u) < 2:
+                pass
+            else:
+                continue
+        yield from dfs_step(G, [*path, u], small_twice)
 
 
 def part_1(lines: List[str]) -> int:
     G = parse(lines)
-    paths = list(find_paths(G))
+    paths = list(dfs_step(G, ["start"]))
+    return len(paths)
+
+
+def part_2(lines: List[str]) -> int:
+    G = parse(lines)
+    paths = list(dfs_step(G, ["start"]))
+    small_caves = [u for u in G if u.islower() and u not in {"start", "end"}]
+    for small in small_caves:
+        paths.extend(dfs_step(G, ["start"], small))
+    # for path in sorted(paths):
+    #     print(",".join(path))
     return len(paths)
