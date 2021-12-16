539cd23218d4897dbfef2e7c6e020293e99484f8
yut23 <yut23@gvljohnsons.com>
2021-12-16 15:00:34 -0500

    Day 14, part 2

--- src/day14.py
+++ src/day14.py
@@ -1,31 +1,62 @@
 from collections import Counter
 from typing import Dict, List, Tuple
 
-# (first element, second element) -> inserted element
-Rules = Dict[Tuple[str, str], str]
+Pair = Tuple[str, str]
 
 
-def parse(lines: List[str]) -> Tuple[List[str], Rules]:
-    template = list(lines[0])
-    rules: Rules = {}
-    for line in lines[2:]:
-        pair, _, insert = line.partition(" -> ")
-        rules[pair[0], pair[1]] = insert
-    return template, rules
+# We can use a similar trick as day 6, by just tracking the counts for each pair,
+# and ignoring the order within the template.
+# This is possible because each replacement leaves the outer elements unchanged:
+#    ... N C ... => ... N B C ...
+
+# We can get the element counts from the pair counts, since each element will
+# show up in exactly two pairs. We just count up all the elements in each pair
+# and divide by 2. This works for everything but the elements on the ends of the
+# template, since those only appear once. We can store those and add an extra
+# occurrence of each to the raw counts, which fixes the total counts.
+
+
+class Polymer:
+    def __init__(self, lines: List[str]):
+        # the number of occurrences of each pair of elements in the polymer
+        self.pairs: Counter[Pair] = Counter(zip(lines[0], lines[0][1:]))
+        # these are needed for calculating the score
+        self.ends: Tuple[str, str] = lines[0][0], lines[0][-1]
+
+        self.replacements: Dict[Pair, List[Pair]] = {}
+        for line in lines[2:]:
+            pair, _, insert = line.partition(" -> ")
+            first, second = tuple(pair)
+            self.replacements[first, second] = [(first, insert), (insert, second)]
+
+    def update(self) -> None:
+        changes: Counter[Pair] = Counter()
+        changes.subtract(self.pairs)
+        for pair, count in self.pairs.items():
+            first, second = self.replacements[pair]
+            changes[first] += count
+            changes[second] += count
+        self.pairs.update(changes)
+
+    def score(self) -> int:
+        # add an extra occurence for the ends, so we can cleanly divide by 2
+        elements = Counter(self.ends)
+        for (first, second), count in self.pairs.items():
+            elements[first] += count
+            elements[second] += count
+        ordered = elements.most_common()
+        return (ordered[0][1] - ordered[-1][1]) // 2
 
 
 def part_1(lines: List[str]) -> int:
-    template, rules = parse(lines)
-    # print(f"Template:     {''.join(template)}")
+    polymer = Polymer(lines)
     for _ in range(10):
-        to_insert = []
-        for pair in zip(template, template[1:]):
-            to_insert.append(rules[pair])
-        # interleave to_insert with template
-        new_template = template + to_insert
-        new_template[::2] = template
-        new_template[1::2] = to_insert
-        template = new_template
-        # print(f"After step {_ + 1}: {''.join(template)}")
-    counts = sorted(Counter(template).values())
-    return counts[-1] - counts[0]
+        polymer.update()
+    return polymer.score()
+
+
+def part_2(lines: List[str]) -> int:
+    polymer = Polymer(lines)
+    for _ in range(40):
+        polymer.update()
+    return polymer.score()
