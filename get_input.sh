01435610b4cf1247d5280213e422b0cc28ea1f93
yut23 <yut23@gvljohnsons.com>
2021-12-20 13:41:59 -0500

    get_input.sh: fail cleanly if .aoc_session is missing

--- get_input.sh
+++ get_input.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 day=${1:-$(date +%-e)}
-curl "https://adventofcode.com/2021/day/$day/input" --cookie "session=$(<.aoc_session)" > "input/day$day.txt"
+session=$(<.aoc_session)
+curl "https://adventofcode.com/2021/day/$day/input" --cookie "session=$session" > "input/day$day.txt"
