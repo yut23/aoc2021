ead707043549e4281b99558e37f5cea4fb0fcec2
yut23 <yut23@gvljohnsons.com>
2021-12-21 17:53:57 -0500

    Day 21, part 2

--- src/day21.py
+++ src/day21.py
@@ -1,5 +1,7 @@
-from itertools import cycle, islice
-from typing import Iterator, List, Tuple
+import functools
+from collections import Counter
+from itertools import cycle, islice, product
+from typing import List, Tuple
 
 
 def parse(lines: List[str]) -> Tuple[int, int]:
@@ -8,10 +10,11 @@ def parse(lines: List[str]) -> Tuple[int, int]:
     return player_1, player_2
 
 
-def play(start_1: int, start_2: int, die: Iterator[int]) -> Tuple[int, int, int]:
+def play_deterministic(start_1: int, start_2: int) -> Tuple[int, int, int]:
     "Return the number of die rolls and the winning and losing players' scores."
     positions = [start_1, start_2]
     scores = [0, 0]
+    die = cycle(range(1, 101))
     rolls = 0
     curr_player = 0
     while True:
@@ -28,6 +31,50 @@ def play(start_1: int, start_2: int, die: Iterator[int]) -> Tuple[int, int, int]
 
 def part_1(lines: List[str]) -> int:
     starts = parse(lines)
-    die = cycle(range(1, 101))
-    rolls, _, loser = play(*starts, die)
+    rolls, _, loser = play_deterministic(*starts)
     return rolls * loser
+
+
+dirac_frequencies: List[Tuple[int, int]] = list(
+    Counter(map(sum, product(range(1, 4), repeat=3))).items()  # type: ignore
+)
+
+
+@functools.lru_cache(maxsize=None)
+def step_dirac(
+    pos_curr: int,
+    pos_other: int,
+    score_curr: int = 0,
+    score_other: int = 0,
+    curr_player: int = 0,
+) -> List[int]:
+    wins = [0, 0]
+    for roll, count in dirac_frequencies:
+        new_pos = (pos_curr + roll) % 10
+        new_score = score_curr + new_pos + 1
+        if new_score >= 21:
+            wins[curr_player] += count
+            continue
+        new_wins = step_dirac(
+            pos_other,
+            new_pos,
+            score_other,
+            new_score,
+            1 - curr_player,
+        )
+        wins[0] += new_wins[0] * count
+        wins[1] += new_wins[1] * count
+    return wins
+
+
+def play_dirac(start_1: int, start_2: int) -> List[int]:
+    "Return the win counts for each player."
+    # clear the cache for timing measurements
+    step_dirac.cache_clear()
+    return step_dirac(start_1 - 1, start_2 - 1)
+
+
+def part_2(lines: List[str]) -> int:
+    starts = parse(lines)
+    wins = play_dirac(*starts)
+    return max(wins)
